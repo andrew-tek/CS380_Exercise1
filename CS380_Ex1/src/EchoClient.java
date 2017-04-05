cb9582bd8367b14a85abe4a9144271bf986ec3b8
Andrew Tek <andrewtek@gmail.com>
2017-04-04 22:18:56 -0700

    final

--- CS380_Ex1/src/EchoClient.java
+++ CS380_Ex1/src/EchoClient.java
@@ -9,7 +9,6 @@ public final class EchoClient {
 
     public static void main(String[] args) throws Exception {
         try (Socket socket = new Socket("localhost", 22222)) {
-        	System.out.println(socket);
             InputStream is = socket.getInputStream();
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
--- CS380_Ex1/src/EchoServer.java
+++ CS380_Ex1/src/EchoServer.java
@@ -6,40 +6,42 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public final class EchoServer {
-//private static volatile Socket socket;
+	public static class ClientMultiThread implements Runnable {
+		Socket socket;
+		public ClientMultiThread (Socket socket) {
+			super();
+			this.socket = socket;
+		}
+		@Override
+		public void run() {
+			try {
+                String address = socket.getInetAddress().getHostAddress();
+                System.out.printf("Client connected: %s%n", address);
+                OutputStream os = socket.getOutputStream();
+                PrintStream out = new PrintStream(os, false, "UTF-8");
+                out.printf("Hi %s, thanks for connecting!%n", address);
+                String inputLine = "";
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while ((inputLine = in.readLine()) != null) {
+                	out.println("Server> " + inputLine);
+                }
+                System.out.printf("Client disconnected: %s%n", address); 
+            	}
+            	catch (Exception e) {
+            		System.out.println("Exception: " + e);
+            	}
+		}
+	}
     public static void main(String[] args) throws Exception {
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
             while (true) {
-                try ( Socket socket = serverSocket.accept()) {
-                	
-                	Runnable client = () -> {
-                	try {
-                	System.out.println("HERE");
-                    String address = socket.getInetAddress().getHostAddress();
-                	System.out.println("HERE1");
-                    System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                	System.out.println("HERE2");
-                    PrintStream out = new PrintStream(os, false, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
-                    String inputLine = "";
-                	System.out.println("HERE3");
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    while ((inputLine = in.readLine()) != null) {
-                    	System.out.println(inputLine);
-                    	out.println("Server> " + inputLine);
-                    }
-                	System.out.println("HERE4");
-                    System.out.printf("Client disconnected: %s%n", address); 
-                	}
-                	catch (Exception e) {
-                		System.out.println("Exception: " + e);
-                	}
-                	};
-                	Thread clientThread = new Thread (client);
-                	clientThread.start();
-            
-                }
+            	try  {
+					Thread t= new Thread(new ClientMultiThread(serverSocket.accept()));
+					t.start();
+				}
+				catch(Exception e){
+				}
+            	
             }
         }
     }
